aae213e7d76e15efb34336c75e695d5b78928a8f
zhaozhaojiejie <2272280759@qq.com>
2020-03-06 18:01:02 +0800

    第二：配置默认样式和静态图片资源

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import ElementUI from 'element-ui'// 1.移入element-ui
+import './styles/index.less' // 3.引入初始化全局样式
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI)// 2.注册ElementUI
 new Vue({
   router,
   render: h => h(App)
